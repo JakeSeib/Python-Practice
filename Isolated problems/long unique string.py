494b5e6d667942ac83f6210a58a67108e59031a2
JakeSeib <33760827+JakeSeib@users.noreply.github.com>
2019-12-01 15:27:10 -0500

    practice problems relating to substrings

---
+++ Isolated problems/long unique string.py
@@ -0,0 +1,34 @@
+def find_non_repeating(line, current_substring, substring_list, substring_num):
+    """
+        the length of the longest substring without repeating chars
+    """
+    if line[0] in current_substring:
+        if len(line)>1:
+            substring_list.append((current_substring, len(current_substring), substring_num))
+            substring_num += 1
+            return find_non_repeating(line[1:], current_substring[(current_substring.index(line[0])) + 1:] + line[0], substring_list, substring_num)
+        else:
+            substring_list.append((current_substring, len(current_substring), substring_num))
+    else:
+        substring = current_substring + line[0]
+        if len(line)>1:
+            return find_non_repeating(line[1:], substring, substring_list, substring_num)
+        else:
+            substring_list.append((substring, len(substring), substring_num))
+    return substring_list
+
+def non_repeat(line):
+    """
+        the longest substring without repeating chars
+    """
+    if line:
+        return sorted(find_non_repeating(line, '', [], 0), key=lambda x: (-x[1], x[2]))[0][0]
+    else:
+        return ''
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert non_repeat('aaaaa') == 'a', "First"
+    assert non_repeat('abdjwawk') == 'abdjw', "Second"
+    assert non_repeat('abcabcffab') == 'abcf', "Third"
+    print('"Run" is good. How is "Check"?')
